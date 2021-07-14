9e64c994c43a37310958ff265adeaae79463aa0a
couspor <theavinashmishra13@gmail.com>
2021-07-14 14:56:04 +0530

    minor changes - removed unwanted files

--- src/sections/blogs.js
+++ src/sections/blogs.js
@@ -4,7 +4,7 @@ import Masonry from "react-masonry-component";
 import BlockTitle from "components/block-title";
 import BlogCard from "components/cards/blog-card";
 
-import blogImage1 from "assets/blog-1-1.png";
+import blogImage1 from "assets/blog-1-1.jpg";
 import blogImage2 from "assets/blog-1-2.png";
 import blogImage3 from "assets/blog-1-3.png";
 import blogImage4 from "assets/blog-1-4.png";
